0169791bfd82c92ce960a5d89c5eaf2cdc09232e
Julius Karl <julius.karl.macrohon@gmail.com>
2025-02-12 10:37:14 +1300

    implement middlewares

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 
 import { PORT } from './config/env.js'; 
 
@@ -6,16 +6,26 @@ import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscriptions.routes.js'
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './models/middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(errorMiddleware);
+app.use(urlencoded({extended: false}));
+app.use(cookieParser())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send({body: 'Welcome to the subscription tracker API!'});
+})
 app.use('/api/v1/auth', authRouter);
 app.use('/users', userRouter);
 app.use('/subscriptions', subscriptionRouter);
 
-app.get('/', (req, res) => {
-    res.send({body: 'Welcome to the subscription tracker API!'});
-})
+
 
 app.listen(PORT, async () => {
     console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
